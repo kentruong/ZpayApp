74521768694518479e49a7753e52f8791dd190d7
Ken Truong <anhkhoaprotruong@gmail.com>
2020-05-26 22:57:36 +0700

    navigation
    by Ken

--- ZpayApp/App.js
+++ ZpayApp/App.js
@@ -25,31 +25,56 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 // import Home from './screens/Home';
-// import Robot from './components/Robot';
-// import MultipleGreetings from './components/MultipleGreetings';
-// import Home from "./screens/Home/Home"
-import MainCategory from "./screens/Category/MainCategory"
-// import Demo from "./screens/Demo"
+import Robot from './components/Robot';
+import MultipleGreetings from './components/MultipleGreetings';
+import Home from "./screens/Home/Home"
+// import MainCategory from './screens/Category/MainCategory';
+import LayoutManageCard from "./screens/ManageCard/LayoutManageCard"
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+    );
+}
+
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+    return (
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    );
+}
 
 class App extends Component {
     render() {
         return (
             // <View style={styles.wrapper}>
-            //     <Robot />
-            //     <MultipleGreetings />
-            //     <Home />
+            //     <ScrollView style={styles.scrollView} >
+            //         <MainCategory />
+            //     </ScrollView>
             // </View>
-            <View style={styles.wrapper}>
-                <ScrollView style={styles.scrollView} >
-                    {/* <Home /> */}
-                    <MainCategory />
-                </ScrollView>
-
-
-                {/* <Demo /> */}
-            </View>
-
-        )
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name="Home" component={Home} />
+                    <Tab.Screen name="Settings" component={MultipleGreetings} />
+                    <Tab.Screen name="Card" component={LayoutManageCard} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        );
     }
 }
 
@@ -60,5 +85,5 @@ const styles = StyleSheet.create({
     scrollView: {
         // backgroundColor: "pink"
     },
-})
+});
 export default App;
--- ZpayApp/screens/ListCard.js
+++ ZpayApp/screens/ListCard.js
@@ -1,129 +1,129 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-  StyleSheet,
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  ImageBackground,
-  Image,
+    StyleSheet,
+    View,
+    Text,
+    FlatList,
+    TouchableOpacity,
+    ImageBackground,
+    Image,
 } from 'react-native';
 
 import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import ArrayCard from '../datas/arrayCard';
 
-const Item = ({hour, icon, degress}) => {
-  return (
-    <View style={styles.itemsListCard}>
-      {/* <IconAntDesign style={styles.iconTransaction} name={icon} size={30} />
+const Item = ({ hour, icon, degress }) => {
+    return (
+        <View style={styles.itemsListCard}>
+            {/* <IconAntDesign style={styles.iconTransaction} name={icon} size={30} />
       <IconAntDesign style={styles.iconTransaction} name={icon} size={30} /> */}
-      {/* <View
+            {/* <View
         style={{
           padding: 8,
         }}> */}
-        <Text style={styles.infoItems}>{hour}</Text>
-        <IconFontAwesome5 style={styles.iconTransaction} name={icon} size={30} />
-        <Text style={styles.infoItems}>{degress}°C</Text>
-      {/* </View> */}
-    </View>
-  );
+            <Text style={styles.infoItems}>{hour}</Text>
+            <IconFontAwesome5 style={styles.iconTransaction} name={icon} size={30} />
+            <Text style={styles.infoItems}>{degress}°C</Text>
+            {/* </View> */}
+        </View>
+    );
 };
 
 class ListCard extends Component {
-  _doWork = param => () => {
-    console.log(param);
-  };
-  render() {
-    return (
-      <View style={styles.containerListCard}>
-        <View style={styles.screensBackground}>
-          <Image
-            style={styles.ImagesScreensBackground}
-            source={{
-              uri:
-                'https://images.unsplash.com/photo-1564223288351-a96bea22b5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=670&q=80',
-            }}
-          />
-        </View>
-        <Text style={{color:"#fff",margin:12, fontSize: 21, fontWeight: "bold"}}>Weather forecasts</Text>
-        <View style={styles.rowListCard}>
-          <FlatList
-            style={styles.flatListListCard}
-            horizontal={true}
-            data={ArrayCard}
-            renderItem={({item, index}) => (
-              <View style={styles.arrayTransaction}>
-                <Item
-                  hour={item.hour}
-                  degress={item.degress}
-                  icon={item.icon}
-                />
-              </View>
-            )}
-            keyExtractor={(item, index) => {
-              return index.toString();
-            }}
-          />
-        </View>
-      </View>
-    );
-  }
+    _doWork = param => () => {
+        console.log(param);
+    };
+    render() {
+        return (
+            <View style={styles.containerListCard}>
+                <View style={styles.screensBackground}>
+                    <Image
+                        style={styles.ImagesScreensBackground}
+                        source={{
+                            uri:
+                                'https://images.unsplash.com/photo-1564223288351-a96bea22b5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=670&q=80',
+                        }}
+                    />
+                </View>
+                <Text style={{ color: "#fff", margin: 12, fontSize: 21, fontWeight: "bold" }}>Weather forecasts</Text>
+                <View style={styles.rowListCard}>
+                    <FlatList
+                        style={styles.flatListListCard}
+                        horizontal={true}
+                        data={ArrayCard}
+                        renderItem={({ item, index }) => (
+                            <View style={styles.arrayTransaction}>
+                                <Item
+                                    hour={item.hour}
+                                    degress={item.degress}
+                                    icon={item.icon}
+                                />
+                            </View>
+                        )}
+                        keyExtractor={(item, index) => {
+                            return index.toString();
+                        }}
+                    />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  screensBackground: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-  },
-  ImagesScreensBackground: {
-    flex: 1,
-    flexDirection: 'column',
-    width: null,
-    height: null,
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-  },
-  containerListCard: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  rowListCard: {
-    height: 150,
-  },
-  itemsListCard: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: "space-between",
-    width: 90,
-    // height: 100,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-    marginHorizontal: 4,
-    marginVertical: 8,
-    padding: 8
-  },
-  ImageCard: {
-    width: 350,
-    height: '100%',
-    backgroundColor: 'gray',
-    opacity: 1,
-  },
-  iconTransaction: {
-    color: '#fff',
-  },
-  infoItems: {
-    color: '#fff',
-  },
-  flatListListCard: {
-    backgroundColor: 'gray',
-    opacity: 0.8,
-  },
+    screensBackground: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+    },
+    ImagesScreensBackground: {
+        flex: 1,
+        flexDirection: 'column',
+        width: null,
+        height: null,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+    },
+    containerListCard: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    rowListCard: {
+        height: 150,
+    },
+    itemsListCard: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: "space-between",
+        width: 90,
+        // height: 100,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff',
+        marginHorizontal: 4,
+        marginVertical: 8,
+        padding: 8
+    },
+    ImageCard: {
+        width: 350,
+        height: '100%',
+        backgroundColor: 'gray',
+        opacity: 1,
+    },
+    iconTransaction: {
+        color: '#fff',
+    },
+    infoItems: {
+        color: '#fff',
+    },
+    flatListListCard: {
+        backgroundColor: 'gray',
+        opacity: 0.8,
+    },
 });
 
 export default ListCard;
